e768479facf5b55fcd060a46c0acafad3126b0ea
Mithras Kuipers <mithraskuipers@gmail.com>
2024-06-19 11:16:32 +0200

    init

---
+++ async-await/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    // Async task1()
+    public static async Task Task1()
+    {
+        Console.WriteLine("Start task1");
+        await Task.Delay(2000);
+        Console.WriteLine("End task1");
+    }
+
+    // Async task2()
+    public static async Task Task2()
+    {
+        Console.WriteLine("Start task2");
+        await Task.Delay(1000);
+        Console.WriteLine("End task2");
+    }
+
+    // Main to run asyncs
+    public static async Task Main(string[] args)
+    {
+        await Task.WhenAll(Task1(), Task2());
+    }
+}
